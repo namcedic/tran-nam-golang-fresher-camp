c5fcb6b22a3210059eded81cc774afc26f96c72a
Namcedic <81761970+namcedic@users.noreply.github.com>
2022-02-17 11:27:24 +0700

    Delete modules directory

--- modules/food/foodmodel/filter.go
+++
@@ -1,5 +0,0 @@
-package foodmodel
-
-type Filter struct {
-	RestaurantId int `json:"restaurant_id,omitempty" form:"restaurant_id"`
-}
--- modules/userlike/model/filter.go
+++
@@ -1,6 +0,0 @@
-package userlikemodel
-
-type Filter struct {
-	RestaurantId int `json:"-" form:"restaurant_id"`
-	UserId       int `json:"-" form:"user_id"`
-}
